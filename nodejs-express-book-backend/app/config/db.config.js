51776477802e674c8e2bb586ddb18c3965c131c0
thesh96 <38894750+thesh96@users.noreply.github.com>
2022-11-17 09:46:38 -0800

    Update db.config.js

--- nodejs-express-book-backend/app/config/db.config.js
+++ nodejs-express-book-backend/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-    HOST: "database-1.c0fs7x3qetzl.us-east-1.rds.amazonaws.com",
-    USER: "root",
-    PASSWORD: "msdhoni07",
-    DATABASE: "webservicedb",
-    PORT: 3306
+    HOST: "",
+    USER: "",
+    PASSWORD: "",
+    DATABASE: "",
+    PORT: 
 };
